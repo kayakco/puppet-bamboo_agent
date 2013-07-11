ff576b6b2ea52041fdbadd24aa3877b169e60f6c
chqr <choover@kayak.com>
2013-07-11 18:10:20 -0400

    First pass at travis configration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
 require 'rubygems'
-require 'puppet'
-require 'facter'
-require 'mocha/api'
-gem 'rspec', '>=2.0.0'
-require 'rspec/expectations'
-
 require 'puppetlabs_spec_helper/module_spec_helper'
 
