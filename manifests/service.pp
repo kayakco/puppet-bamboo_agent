1049147fa0dc5682bc0c72fc56e57b24c898f0e8
chqr <choover@kayak.com>
2013-08-21 11:02:03 -0400

    Add documentation to service type

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,10 @@
+# Declares the agent a Puppet service and ensures it is running and enabled,
+# after rendering an init script that delegates to the agent's bamboo-agent.sh
+# script.
+# *** This type should be considered private to this module ***
 define bamboo_agent::service(
   $home,
-  $id    = $title,
+  $id = $title,
 ){
 
   $service = "bamboo-agent${id}"
